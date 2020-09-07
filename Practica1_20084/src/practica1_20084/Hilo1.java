b776a9a2a615d262b18f0bda97864904e9fffca9
SDavidGS <70719492+SDavidGS@users.noreply.github.com>
2020-09-07 17:54:01 -0500

    Add files via upload

---
+++ Practica1_20084/src/practica1_20084/Hilo1.java
@@ -0,0 +1,46 @@
+package practica1_20084;
+import javax.swing.JTextArea;
+
+public class Hilo1 extends Thread{
+    private JTextArea area;
+    private boolean pausado = false;
+    private boolean corriendo = true;
+    
+    public Hilo1(JTextArea area){
+        this.area=area;
+    }
+    @Override
+    public void run(){
+        try{
+            int i = 0;
+            while(true){
+                area.append(i + "\n");
+                i++;
+                sleep(1500);
+                synchronized(this){
+                    if(pausado)
+                        wait();
+                    if(!corriendo)
+                        join();
+                }
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }    
+    }
+    
+    public void pausar(){
+        this.pausado = true;
+    }
+    
+    public void continuar(){
+        synchronized(this){
+            pausado = false;
+            notifyAll();
+        }
+    }
+    
+    public void parar(){
+        this.corriendo = false;
+    }
+}
